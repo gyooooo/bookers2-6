5c017673fa8cebe43c8a66cd38cc521797b6e56e
gyooooo <harapeko2925@gmail.com>
2022-07-06 05:37:51 +0000

    fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_comment = BookComment.new
+    @books = BookComment.all
     # @books = Book.
   end
 
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,14 @@ class FavoritesController < ApplicationController
       favorite = current_user.favorites.new(book_id: book.id)
       favorite.save
       redirect_to book_path(book)
+      # render :show
     end
     
     def destroy
       book = Book.find(params[:book_id])
       favorite = current_user.favorites.find_by(book_id: book.id)
       favorite.destroy
-      render :show
-    #   redirect_to book_path(book)
+      # render :show
+      redirect_to book_path(book)
     end
 end
