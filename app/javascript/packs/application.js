74c7409e69fbbb361e4bd5764165ef01cf1fce6c
gyooooo <harapeko2925@gmail.com>
2022-10-05 13:18:33 +0000

    javascript スコープ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -67,3 +67,13 @@ var nickname = "taro"
 console.log(nickname)
 nickname = "jiro"
 console.log(nickname)
+
+var str = "webcamp" //グローバルスコープ
+
+function foo() {
+  console.log(str)
+  var y = "hello" //関数スコープ
+}
+
+foo()
+console.log(y)
