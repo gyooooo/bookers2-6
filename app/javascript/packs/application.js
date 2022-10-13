a3156a508aa2ddcc7b32d925321039aefab93698
gyooooo <harapeko2925@gmail.com>
2022-10-13 14:55:05 +0000

    check

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -185,14 +185,14 @@ $(document).ready(function () {
 //   $('.box1').slideDown();
 // });
 
-// $(function(){
-//   $('.box1').mouseover(function(){
-//     $('.box1').addClass('box1-ext');
-//   });
-//   $('.box1').mouseout(function(){
-//     $('.box1').removeClass('box1-ext');
-//   });
-// });
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
 
 
 
