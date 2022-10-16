6bb550f0034183f001c4b1d49022758b104716d7
gyooooo <harapeko2925@gmail.com>
2022-10-16 14:57:21 +0000

    じゃんけん

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,45 +22,45 @@ let str1 = 'Hello';
 let str2 = 'World!!';
 alert(str1 + str2);
 
-// let orange = 100;
-// let apple = 120;
+let orange = 100;
+let apple = 120;
 
-// if(orange < apple){
-//   alert('みかんの値段がりんごより安い');
-// } else if(orange == apple){
-//   alert('みかんとりんごが同じ値段');
-// } else{
-//   alert('みかんの値段がりんごより高い');
-// }
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
 
-// let max = 100;
-// let num = 1;
-// let count = 0;
+let max = 100;
+let num = 1;
+let count = 0;
 
-// while(num < max){
-//   num = num * 2;
-//   count = count + 1;
-// }
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
 
-// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
-// // varによる再宣言
-// var nickname = "taro"
-// console.log(nickname)
-// var nickname = "ichiro"
-// console.log(nickname)
+// varによる再宣言
+var nickname = "taro"
+console.log(nickname)
+var nickname = "ichiro"
+console.log(nickname)
 
-// letによる再宣言
-// let nickname = "taro"
-// console.log(nickname)
-// let nickname = "ichiro"
-// console.log(nickname)
+letによる再宣言
+let nickname = "taro"
+console.log(nickname)
+let nickname = "ichiro"
+console.log(nickname)
 
-// constによる再宣言
-// const nickname = "taro"
-// console.log(nickname)
-// const nickname = "ichiro"
-// console.log(nickname)
+constによる再宣言
+const nickname = "taro"
+console.log(nickname)
+const nickname = "ichiro"
+console.log(nickname)
 
 // varによる再代入
 var nickname = "taro"
